ba75b76a6b6331e6e0ff0c14ddfd844f304af421
김성현 <egoavara@gmail.com>
2021-12-21 23:02:04 +0900

    2021-12-21 커밋

--- Programmers/C30L1836/C30L1836.cpp
+++ Programmers/C30L1836/C30L1836.cpp
@@ -1,21 +1,16 @@
 #include <algorithm>
-#include <cctype>
-#include <map>
+#include <iostream>
 #include <string>
 #include <vector>
 
 using namespace std;
-
+#define MAT2D_POS(w, x, y) (w * y + x)
 // 전역 변수를 정의할 경우 함수 내에 초기화 코드를 꼭 작성해주세요.
 string solution(int m, int n, vector<string> board) {
-    map<char, vector<pair<int, int>>> locs;
-    for (size_t row = 0; row < m; row++) {
-        for (size_t col = 0; col < n; col++) {
-            if (isalpha(board[row][col])) {
-                locs[board[row][col]].push_back(make_pair(row, col));
-            }
-        }
-    }
+    vector<char> board2d(m * n);
+    for_each(board.cbegin(), board.cend(), [&, i = 0](string a) mutable {
+        for_each(a.cbegin(), a.cend(), [&](char c) { board2d[i++] = c; });
+    });
 
     string answer = "";
     return answer;
