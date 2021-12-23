30fc5d73102de2d5c9d8a4f8d7047b8197dc99b7
김성현 <egoavara@gmail.com>
2021-12-23 22:30:14 +0900

    프로그래머스 C30L86491 풀이

--- Programmers/C30L86491/C30L86491.py
+++ Programmers/C30L86491/C30L86491.py
@@ -1,13 +1,23 @@
 # %%
-import math
 def solution(sizes):
-    cx, cy = math.inf, math.inf
+    cx, cy = 0, 0
     for x, y in sizes:
-        nw, nh = cx > x, cy > y
-        nwh = nw and nh
-        rw, rh = cy > x, cx > y
-        rwh = rw and rh
-        if nwh or rwh:
-            continue
-    return answer
+        nw, nh = cx < x, cy < y
+        nwh = nw or nh
+        rw, rh = cy < x, cx < y
+        rwh = rw or rh
+        if nwh and rwh:
+            nx, ny = max(cx, x), max(cy, y)
+            nsz = nx * ny
+            rx, ry = max(cx, y), max(cy, x)
+            rsz = rx * ry
+            if nsz <= rsz:
+                cx, cy = nx, ny
+            else:
+                cx, cy = rx, ry
+    return cx * cy
+
+
 print(solution([[60, 50], [30, 70], [60, 30], [80, 40]]))
+print(solution([[10, 7], [12, 3], [8, 15], [14, 7], [5, 15]]))
+print(solution([[14, 4], [19, 6], [6, 16], [18, 7], [7, 11]]))
